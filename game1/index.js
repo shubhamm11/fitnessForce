c094ebd461d59fbda281e2040656cc9049aec659
Shubham Matwandkar <shubhamm478@gmail.com>
2019-06-25 17:53:16 -0700

    Updated

---
+++ game1/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".mainDiv").mouseenter(function(){
+        var x = event.clientX;
+        var y =event.clientY;
+        console.log(x+" : "+y);
+        // alert(x+" : "+y);
+    });
+    
+
+});
